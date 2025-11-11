18c971db0858b3f1da061471e868aa709bf22227
Victor Caldas Nery <victor.nery@sipam.gov.br>
2025-11-11 17:41:41 -0300

    Frontend do noSQL feito

--- src/main/java/org/example/ucb/clinica_api/ClinicaApiApplication.java
+++ src/main/java/org/example/ucb/clinica_api/ClinicaApiApplication.java
@@ -2,7 +2,6 @@ package org.example.ucb.clinica_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.example.ucb.clinica_api.control.RepositorioDeDono;
