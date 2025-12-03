ce2c61b5e3afd59fef2577909196538b6a4a5ce6
Renan Watanabe <renanwatanabe802@gmail.com>
2025-12-03 17:03:39 -0300

    ON DELETE CASCADE para animal e consultas

--- App_clinica_vet/updates.sql
+++ App_clinica_vet/updates.sql
@@ -14,3 +14,10 @@ ALTER TABLE tratamento
 ADD CONSTRAINT FK_id_consulta
 	FOREIGN KEY (id_consulta) REFERENCES consulta(id)
     ON DELETE CASCADE;
+
+ALTER TABLE consulta DROP FOREIGN KEY FK_Animal_Consulta;
+
+ALTER TABLE consulta
+ADD CONSTRAINT Fk_Animal_Consulta
+FOREIGN KEY (id_animal) REFERENCES Animal(RFID)
+ON DELETE CASCADE;
